f27ee004cb4ffca98e5d25a9942d182a1c2f42f5
Unknown <joeymays94@gmail.com>
2019-04-12 15:54:55 -0400

    Revamped Day1 and Day2 R Notebooks

---
+++ Scripts/MetadataInit.R
@@ -0,0 +1,21 @@
+### 10X Genomics Initial Metadata Processing 
+#Input: Seurat Object v2.3.4
+#Stores BARCODES in Metadata table column 'barcode'
+#Creates Cell IDs from a given prefix and numbers in increments of 1
+#Stores Cell IDs in Metadata Table
+### ///////////////////////////////
+
+MetadataInit <- function(SeuratObject, ID.prefix){
+  
+  # Save Cell IDs to MetaData Slot
+  barcodes <- colnames(SeuratObject@raw.data)
+  SeuratObject@meta.data[,"barcode"] <- barcodes
+  
+  cell.ids = c()
+  for(i in 1:length(colnames(SeuratObject@raw.data))){
+    cell.id.current <- paste(ID.prefix, i, sep = '_')
+    cell.ids <- c(cell.ids, cell.id.current)
+  }  
+  SeuratObject@meta.data[,"cell.id"] <- cell.ids
+  return(SeuratObject)
+}
---
+++ Scripts/SetupDependencies.R
@@ -0,0 +1,4 @@
+library(Seurat) #v2.3.4
+library(ggplot2)
+library(dplyr)
+source("./Scripts/MetadataInit.R") #Loads function for metadata handling
